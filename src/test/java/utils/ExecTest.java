32b29b362607a889639e5f2d488ec703537064b6
Hack-Hut <l-goddard@protonmail.com>
2019-09-08 14:00:18 +0100

    fixed windows unit tests

--- src/test/java/utils/ExecTest.java
+++ src/test/java/utils/ExecTest.java
@@ -4,15 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ExecTest {
+    private String os = utils.SystemOps.getOsType().toLowerCase();
 
     @Test
     void executeCommandGetOutputRealCommand() {
-        String[] cmd = {"echo", "testing"};
-        ArrayList<String> output = utils.Exec.executeCommandGetOutput(cmd);
-        assertEquals("testing", output.get(0));
+        if(os.equals("linux")) {
+            String[] cmd = {"echo", "testing"};
+            ArrayList<String> output = utils.Exec.executeCommandGetOutput(cmd);
+            assertEquals("testing", output.get(0));
+        }
+        else{
+            String[] cmd = {"cmd.exe", "/c", "echo", "testing"};
+            ArrayList<String> output = utils.Exec.executeCommandGetOutput(cmd);
+            assertEquals("testing", output.get(0));
+        }
+
     }
 
     @Test
