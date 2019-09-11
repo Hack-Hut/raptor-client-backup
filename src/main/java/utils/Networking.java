7b13cadfd4a7040e0ed18723fb804a458d930851
Hack-Hut <l-goddard@protonmail.com>
2019-09-11 20:00:47 +0100

    fixed ip address issue

--- src/main/java/utils/Networking.java
+++ src/main/java/utils/Networking.java
@@ -1,22 +1,32 @@
 package utils;
 
 
-
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class Networking {
     public static String getLocalIP(){
-        InetAddress ip;
-        try {
-            ip = InetAddress.getLocalHost();
-            return ip.getHostAddress();
 
-        } catch (UnknownHostException e) {
-            Log.error("Failed to get the local IP address");
+        try {
+            Enumeration Interfaces = NetworkInterface.getNetworkInterfaces();
+            while (Interfaces.hasMoreElements()) {
+                NetworkInterface Interface = (NetworkInterface) Interfaces.nextElement();
+                Enumeration Addresses = Interface.getInetAddresses();
+                while (Addresses.hasMoreElements()) {
+                    InetAddress Address = (InetAddress) Addresses.nextElement();
+                    if (Address.getHostAddress().contains("192.")) {
+                        return Address.getHostAddress();
+                    }
+
+                }
+            }
+        }catch (SocketException e){
+            Log.error("Failed getting the local IP address.");
             Log.error(e.toString());
-            return "";
         }
+        return "";
     }
 }
 
